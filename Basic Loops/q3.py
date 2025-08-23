bbcc62e05117e9afda5d4d706b11c72457d77d6f
madhavwaj <bhardwajmadhav003@gmail.com>
2025-08-24 01:46:28 +0530

    Added loops

---
+++ Basic Loops/q3.py
@@ -0,0 +1,5 @@
+# Write a program to print the first 10 odd numbers.
+
+for i in range(1, 11):
+    print(i*2 - 1)
+
---
+++ Basic Loops/q4.py
@@ -0,0 +1,6 @@
+# Write a program to calculate the sum of numbers from 1 to 100.
+
+sum = 0
+for i in range(1,101):
+    sum = sum+i
+    print(sum)
