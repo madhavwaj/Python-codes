88e60e509a584eaddcd0f50be9701056221beacf
Madhav Bhardwaj <118822264+madhavwaj@users.noreply.github.com>
2025-12-21 20:52:29 +0530

    Delete Basic Loops directory

--- Basic Loops/q1.py
+++
@@ -1,4 +0,0 @@
-# Write a program to print the numbers from 1 to 10.
-
-for i in range (1, 11):
-    print(i)
--- Basic Loops/q2.py
+++
@@ -1,4 +0,0 @@
-# Write a program to print the first 10 even numbers.
-
-for i in range(1, 11):
-    print(i*2)
