7810b4f4d55531a12744d2d3400849ae00cd5dfe
Madhav Bhardwaj <118822264+madhavwaj@users.noreply.github.com>
2026-03-10 01:32:04 +0530

    Add files via upload

---
+++ q34.py
@@ -0,0 +1,22 @@
+#Print first 25 prime numbers
+
+def print_primes(n):
+    count = 0
+    num = 2
+
+    while count < n:
+        is_prime = True
+
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
+                break
+
+        if is_prime:
+            print(num)
+            count += 1
+
+        num += 1
+
+
+print_primes(25)
